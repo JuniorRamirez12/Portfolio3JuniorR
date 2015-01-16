743af2b126b8e96ca60c49271afb234e64fc2f0e
Junior Ramirez <Juniorr87@yahoo.com>
2015-01-16 13:54:24 -0800

    update on portfolio 3

--- public_html/script.js
+++ public_html/script.js
@@ -1,11 +1,65 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 
-$("document").ready(function(){
-        $(".practice").css("background-color", "pink");
+ $("document").ready(function() {
 
+    $('#replaceWText').bind('click', replaceWText);
+
+    $('#randPara').bind('click', addAPara);
+
+    $('#removePara').bind('click', removeAPara);
+
+    // $(' .yeah') .css('background-color' , 'yellow');
+
+    $('#superHumans').accordion({header: "h3"});
+
+    //$('superHumans').css('width', '400px');
+
+    // $(' .2') .css({'visibility' : 'hidden' , 'position' : 'absolute'});
+
+    // $('div:last p:nth-child(3)').css('backgroound-color', 'pink');
+
+    $("h1").bind('mouseover', mouseOverMe).bind('mouseout', mouseOutMe);
+
+    $('div').bind('mouseover', mouseOverMe());
+
+    $('h1').bind('click', mouseClick);
+
+    $('#replaceWText').bind('click', replaceWText);
+
+    $('#randPara').bind('click', addAPara);
+
+    $('#removePara').bind('click', removeAPara);
 });
+
+function removeAPara() {
+    $('#randPara p:last').remove();
+}
+
+function addAPara() {
+    $('#randPara').append('<p>ADDED</p>');
+}
+function replaceWText() {
+}
+
+
+
+function mouseOverMe() {
+
+    $("p").html("ILL RAID YOUU");
+
+
+}
+
+function mouseOverMe() {
+
+    $("h1").html("Unknown Lv1 Track Bike");
+}
+
+function mouseOutMe() {
+
+    $("p").html('TRACK BIKES');
+}
+
+function mouseClick (){
+    
+}
